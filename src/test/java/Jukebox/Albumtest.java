462158d2aed2f3a8b259216843a936f8d16ae83e
Euan Hutcheson <euanhutcheson@Euans-MacBook-Pro.local>
2021-10-03 15:39:23 +0100

    third commit

--- src/test/java/Jukebox/Albumtest.java
+++ src/test/java/Jukebox/Albumtest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class Fleetwoodtest {
+public class Albumtest {
 
     @Test
     @DisplayName("Test for Fleetwood Mac album genre")
